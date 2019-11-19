d111e95cc07a45e0eb0475b235e805d5e4bae0f3
lukechang24 <lukechang244@gmail.com>
2019-11-19 11:55:53 -0800

    added api retrieval

--- flask/resources/games.py
+++ flask/resources/games.py
@@ -7,11 +7,25 @@ from playhouse.shortcuts import model_to_dict
 
 game = Blueprint('games', 'game')
 
-@game.route("/", methods=["GET"])
-def get_ten_games():
-    r = requests.get("http://api.steampowered.com/ISteamApps/GetAppList/v0002/?key=STEAMKEY&format=json", data="fields name; limit 100; sort popularity desc; where themes !=(42);", headers={"content-type": "application/json", "user-key": "a3151b1ce686df4983737ccc7f8798e8"})
-    print(jsonify(r.json()), "THIS IS TYPE")
-    return jsonify(games=r.json())
+@game.route("/", methods=["GET", "POST"])
+def get_games():
+    get_all_games = requests.get("http://api.steampowered.com/ISteamApps/GetAppList/v0002/?key=08D497C10BC95B83E82D304CD1293794&format=json", data="fields *; limit 10;", headers={"content-type": "application/json", "user-key": "08D497C10BC95B83E82D304CD1293794"})
+
+    allGames = get_all_games.json()["applist"]["apps"]
+    allIds = [game["appid"] for game in allGames]
+
+    for i, id in enumerate(allIds):
+        if i < 1:
+            get_one_game = requests.get(f"https://store.steampowered.com/api/appdetails?appids={id}", data="fields *; limit 10;", headers={"content-type": "application/json", "user-key": "08D497C10BC95B83E82D304CD1293794"})            
+            retrieved_game = get_one_game.json()[str(id)]["data"]
+            body = {"title": retrieved_game["name"], "genres": retrieved_game["genres"], "creator": retrieved_game["developers"], "release_date": retrieved_game["release_date"], "description": retrieved_game["about_the_game"]}
+            Game = models.Game.create(**body)
+            print(body)
+
+    get_one_game = requests.get("https://store.steampowered.com/api/appdetails?appids=1088487", data="fields *; limit 10;", headers={"content-type": "application/json", "user-key": "08D497C10BC95B83E82D304CD1293794"})
+    return jsonify(get_one_game.json())
+    # return jsonify(r.json()["applist"]["apps"][0:10])
+    # return jsonify(r.json()["1073920"]["data"]["release_date"])
 
 # WE ARE USING THE API JASON FOUND
 # DATA="" is basically the query selector, and headers is where we specify the key and the type of content the api is providing.
